025f8efa3ac79dd8da6dd4974720880664469b5b
ZackeryJBrown <zackeryjbrown@gmail.com>
2022-11-13 10:04:07 -0600

    improved err msg handling

---
+++ includes/dbconn.php
@@ -0,0 +1,18 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    //enter password and database name
+    $password = "";
+    $dbname = "";
+    $conn = "";
+    try{
+        if ($username =="" || $password ==""){
+            throw new Exception ('Action successful, however this demo does not include the connection to the development database. Functionality can be demonstrated in person, or you may review the source code for this site at https://github.com/ZackeryJBrown.');
+        } else {
+            $conn = new mysqli($servername, $username, $password, $dbname);
+        }
+    }
+    catch(Exception $ex) {
+        echo 'Error: '.$ex->getMessage();
+    }
+?>
