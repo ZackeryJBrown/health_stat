27da2ee7ba1fc55678f65a1e7468355737b5e008
ZackeryJBrown <zackeryjbrown@gmail.com>
2022-12-03 15:07:19 -0600

    php 8 support, mysqli -> PDO

--- includes/datahandler.php
+++ includes/datahandler.php
@@ -5,36 +5,28 @@ ini_set('display_errors', '1');
 
 include 'dbconn.php';
 //loaded from dbconn.php
-$conn;
+$pdo;
 
 if (isset ($_POST['incomingData'])){
     $formArray = $_POST['incomingData'];
     $sqlImplode = implode(",", $formArray);
 
-    $sql = 
-    "INSERT INTO bloodpressure (dia , sys, bpm, dateonly)
-    VALUES ($sqlImplode, CURDATE());";
+    //PDO updates
+    $sql = $pdo->prepare("INSERT INTO bloodpressure (dia , sys, bpm, dateonly) VALUES ($sqlImplode, CURDATE());");
+    $sql->execute();
+    $fetch = $sql->fetch();
 
 
-    
-    
-    // if condition runs the query
-    if ($conn->query($sql) === FALSE) {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    if ($sql->rowCount() === 0) {
+        echo "Error";
     } else {
+        echo "SQL returns: " . $sql->rowCount();
         echo "New record created with data: ";
         echo "Sys: " . $formArray["sysValue"] . ", ";
         echo "Dia: " . $formArray["diaValue"] . ", ";
         echo "Pulse: " . $formArray["pulseValue"];
     }
-    
+};
 
-    
-    
-}
-
-
-
-
-$conn->close();
+$dbh=NULL;
 ?>
--- includes/dbconn.php
+++ includes/dbconn.php
@@ -7,7 +7,23 @@
     $password = $passw;
     $dbname = $dbn;
     $conn = "";
-    try{
+    $charset = "utf8mb4";
+    
+    
+    $options = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    $dsn = "mysql:host=$servername;dbname=$dbname;charset=$charset";
+    try {
+         $pdo = new \PDO($dsn, $username, $password, $options);
+    } catch (\PDOException $e) {
+         throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+
+    //mysqli no longer supported
+    /*try{
         if ($username =="" || $password ==""){
             throw new Exception ('Action successful, however this demo does not include the connection to the development database. Functionality can be demonstrated in person, or you may review the source code for this site at https://github.com/ZackeryJBrown.');
         } else {
@@ -16,5 +32,6 @@
     }
     catch(Exception $ex) {
         echo 'Error: '.$ex->getMessage();
-    }
+    }*/
+
 ?>
